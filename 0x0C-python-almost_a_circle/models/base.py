a29eb4a7e39f42f93a2a72b541451df7a295ab4f
OrapelengN <portiaoran@gmail.com>
2024-10-24 02:43:25 -0700

    Add Base class and initial testing

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a base class."""
+
+
+class Base:
+    """Base class to manage the id attribute for future classes."""
+    
+    __nb_objects = 0  # Private class attribute to track number of instances
+
+    def __init__(self, id=None):
+        """Initialize the Base class with an optional id."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
