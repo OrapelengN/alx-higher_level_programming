bc9d5655c889f5b0427adea804bd1881956fc1db
OrapelengN <portiaoran@gmail.com>
2024-10-24 11:50:51 -0700

    Add validation for Rectangle class attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -48,6 +48,10 @@ class Rectangle(Base):
 
         @width.setter
         def width(self, value):
+            if type(value) != int:
+                raise TypeError("width must be an integer"
+            if value < 0:
+                raise ValueError("width must be >= 0")
             self.validate_integer("width", value)
             self.validate_positive("width", value)
             self.__width = value
@@ -59,6 +63,10 @@ class Rectangle(Base):
 
         @height.setter
         def height(self, value):
+            if type(value) != int:
+                raise TypeError("height must be an integer"
+            if value < 0:
+                raise ValueError("height must be >= 0")
             self.validate_integer("height", value)
             self.validate_positive("height", value)
             self.__height = value
@@ -70,6 +78,10 @@ class Rectangle(Base):
 
         @x.setter
         def x(self, value):
+            if type(value) != int:
+                raise TypeError("x must be an integer"
+            if value < 0:
+                raise ValueError("x must be >= 0")
             self.validate_integer("x", value)
             self.validate_non_negative("x", value)
             self.__x = value
@@ -81,6 +93,10 @@ class Rectangle(Base):
 
         @y.setter
         def y(self, value):
+            if type(value) != int:
+                raise TypeError("y must be an integer"
+            if value < 0:
+                raise ValueError("y must be >= 0")
             self.validate_integer("y", value)
             self.validate_non_negative("y", value)
             self.__y = value
