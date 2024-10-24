939e41a754ce7075a65bc1174aa170cec9daaaf5
OrapelengN <portiaoran@gmail.com>
2024-10-24 12:44:57 -0700

    Update display method in Rectangle class to account for x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -49,7 +49,7 @@ class Rectangle(Base):
         @width.setter
         def width(self, value):
             if type(value) != int:
-                raise TypeError("width must be an integer"
+                raise TypeError("width must be an integer")
             if value < 0:
                 raise ValueError("width must be >= 0")
             self.validate_integer("width", value)
@@ -64,7 +64,7 @@ class Rectangle(Base):
         @height.setter
         def height(self, value):
             if type(value) != int:
-                raise TypeError("height must be an integer"
+                raise TypeError("height must be an integeri")
             if value < 0:
                 raise ValueError("height must be >= 0")
             self.validate_integer("height", value)
@@ -79,7 +79,7 @@ class Rectangle(Base):
         @x.setter
         def x(self, value):
             if type(value) != int:
-                raise TypeError("x must be an integer"
+                raise TypeError("x must be an integer")
             if value < 0:
                 raise ValueError("x must be >= 0")
             self.validate_integer("x", value)
@@ -94,9 +94,25 @@ class Rectangle(Base):
         @y.setter
         def y(self, value):
             if type(value) != int:
-                raise TypeError("y must be an integer"
+                raise TypeError("y must be an integer")
             if value < 0:
                 raise ValueError("y must be >= 0")
             self.validate_integer("y", value)
             self.validate_non_negative("y", value)
             self.__y = value
+
+        def area(self):
+            """Return the area of the rectangle"""
+            return self.width * self.height
+
+        def display(self):
+            """Print the rectangle instance using the '#' character with x and y offsets"""
+            for _ in range(self.y):
+                print()
+
+            for _ in range(self.height):
+                print(' ' * self.x + '#' * self.width)
+
+        def __str__(self):
+            """Return the string representation of the Rectangle"""
+            return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
