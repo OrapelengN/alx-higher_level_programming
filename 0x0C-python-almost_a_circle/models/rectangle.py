52b862d304bd6a18a6a5f84d7e74d590ce7f9a64
OrapelengN <portiaoran@gmail.com>
2024-10-25 04:32:57 -0700

    Add to_dictionary method to Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -144,3 +144,13 @@ class Rectangle(Base):
             return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
                                                            self.y, self.width,
                                                            self.height)
+
+        def to_dictionary(self):
+            """Return the dictionary representation of a Rectangle instance"""
+            return {
+                    'id': self.id,
+                    'width': self.width,
+                    'height': self.height,
+                    'x': self.x,
+                    'y': self.y
+                    }
