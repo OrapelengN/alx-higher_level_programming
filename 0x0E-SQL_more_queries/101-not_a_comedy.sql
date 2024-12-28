44ce746a680308656297c403c447f0f371d74779
OrapelengN <portiaoran@gmail.com>
2024-12-28 02:56:51 -0800

    18. No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,11 +2,9 @@
 -- Only one record where name = Comedy (id can be different) must be contained in the tv_genres table
 -- Each record should display: tv_shows.title
 -- Sort results in ascending order by the show title
-SELECT title
+SELECT tv_shows.title
 FROM tv_shows
-WHERE id NOT IN (
-    SELECT show_id
-    FROM tv_show_genres
-    WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy')
-)
-ORDER BY title ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id AND tv_genres.name = 'Comedy'
+WHERE tv_genres.name IS NULL
+ORDER BY tv_shows.title ASC;
