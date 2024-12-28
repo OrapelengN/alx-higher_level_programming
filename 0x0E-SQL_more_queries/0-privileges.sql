b17fd2dd9450ecb618b5a7557594d086b80c30a5
OrapelengN <portiaoran@gmail.com>
2024-12-28 01:23:12 -0800

    0. My privileges!

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- This script lists the privileges of user_0d_1 and user_0d_2 on localhost
+
+-- Show grants for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show grants for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
