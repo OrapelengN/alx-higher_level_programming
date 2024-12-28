3b8219db618982a76143200551412c020beff2c9
OrapelengN <portiaoran@gmail.com>
2024-12-28 03:07:57 -0800

    18. No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -8,4 +8,4 @@ LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name != 'Comedy' OR tv_genres.name IS NULL
 GROUP BY tv_shows.title
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title;
