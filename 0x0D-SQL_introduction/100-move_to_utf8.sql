b2e567fe088a7242406dfd6686b5dc38d45d3355
OrapelengN <portiaoran@gmail.com>
2024-12-27 23:00:29 -0800

    17. Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
--- Select th database
+-- Select the database hbtn_0c_0
 USE hbtn_0c_0;
 
 -- Convert the database hbtn_0c_0 to utf8mb4
@@ -7,5 +7,5 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 -- Convert the table first_table to utf8mb4
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the 'name' column in the table first_table to utf8mb4
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Modify the 'name' column in the table first_table to use utf8mb4_unicode_ci collation (but not explicitly set CHARACTER SET)
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
