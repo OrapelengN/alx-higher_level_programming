06c430da122b03246ba2af51da0f0d86643243fc
OrapelengN <portiaoran@gmail.com>
2024-12-28 00:30:24 -0800

    19. Temperatures #1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,5 @@
+-- Show the top 3 of cities temperature 
+-- Must be during July and August ordered by temperature (descending)
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
 WHERE month IN (7, 8)
