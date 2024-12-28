1f1e6a6a2f13e96354278cc84089ebe126f7d4b6
OrapelengN <portiaoran@gmail.com>
2024-12-27 22:19:04 -0800

    11. Select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- Select and display the score and name columns from second_table
+-- where score is greater than or equal to 10
+-- and order the results by score in descending order
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
