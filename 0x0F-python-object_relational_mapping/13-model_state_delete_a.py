9d98cf70fe82816300c0d3901e9a088a307e2ce5
OrapelengN <portiaoran@gmail.com>
2025-01-02 05:19:16 -0800

    13. Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,47 +1,35 @@
 #!/usr/bin/python3
 """
-Script that deletes all State objects with a name containing the letter 'a'
-from the 'hbtn_0e_6_usa' database.
-The script connects to a MySQL database and deletes records in the 'states'
-table
-where the name contains the letter 'a', using SQLAlchemy for database
-operations.
+Deletes all State objects with a name containing the letter 'a'
+from the database hbtn_0e_6_usa using SQLAlchemy.
 """
-
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 import sys
 
 if __name__ == "__main__":
-    # Take the arguments: mysql username, password, and database name
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Create the engine that will interact with the database
+    # Create engine to connect to MySQL
     engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost/{database}',
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True
     )
-
-    # Create a session to interact with the database
+    # Create a configured session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query the State objects whose name contains 'a' and delete them
+    # Query for states with 'a' in the name
     states_to_delete = (
-        session.query(State)
-        .filter(State.name.like('%a%'))
-        .all()
+            session.query(State).filter(State.name.like('%a%')).all()
     )
 
-    # Loop through the states and delete each one
+    # Delete all matching records
     for state in states_to_delete:
         session.delete(state)
 
-    # Commit the changes
+    # Commit the transaction
     session.commit()
 
-    # Close the session
+    # Close session
     session.close()
