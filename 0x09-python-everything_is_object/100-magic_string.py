68b35e69956099285f6494b7079a3c8c9f3103f4
OrapelengN <portiaoran@gmail.com>
2025-03-19 12:39:42 -0700

    29. #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l.append("BestSchool")
-    return ", ".join(l)
+def magic_string(input_list=[]):
+    input_list.append("BestSchool")
+    return ", ".join(input_list)
