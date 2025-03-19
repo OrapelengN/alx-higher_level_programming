ca3cd34361dd5272cb4a037cf6408a85a55b2a56
OrapelengN <portiaoran@gmail.com>
2025-03-19 12:47:35 -0700

    30. Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+
+"""
+This module defines a LockedClass that restricts dynamic attribute creation,
+allowing only the 'first_name' attribute.
+"""
+
+
 class LockedClass:
     """
     A class that prevents dynamic attribute creation, except for 'first_name'.
