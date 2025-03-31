230752c27abc9c34f0a94f6d069389c313a38256
OrapelengN <portiaoran@gmail.com>
2025-03-31 12:35:23 -0700

    9. Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # This script sends a request to 0.0.0.0:5000/catch_me with the correct header.
+# Trying 99
 curl -s -L -X PUT -H "X-HolbertonSchool-User-Id: 98" 0.0.0.0:5000/catch_me
+
+# If 99 fails, try 100
+# curl -s -L -X PUT -H "X-HolbertonSchool-User-Id: 100" 0.0.0.0:5000/catch_me
